b5062ff98820430154dc20a6882ce60694f6ba0f
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-05-25 15:19:56 -0400

    finalized

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,9 +18,8 @@ module.exports = () => {
       
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE'
+        title: 'Just Another Text Editor'
       }),
-     
       // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -31,8 +30,8 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'JATE',
-        short_name: 'JATE',
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
         description: 'Text Editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
